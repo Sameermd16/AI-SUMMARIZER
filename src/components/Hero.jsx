b24adc1f374d1c6ea49cc93fe23ed6306278aca5
sameermd16 <sameerthemc@gmail.com>
2023-09-16 19:27:46 +0530

    hero - heading & subheading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,10 @@ export default function Hero() {
       </nav>
 
       <h1 className='head_text'>
-        Summarize articles with <br />
-        <span>OpenAi GPT-4</span>
+        Summarize articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
+      <h2 className='desc'>Simplify your reading with Summarize, and open-source article summarizer that transforms lengthy articles into clear and concise summaries</h2>
     </header>
   );
 }
