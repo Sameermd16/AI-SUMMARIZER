ac42c2360fe2f7c6180ffa221d2cde721fc16678
sameermd16 <sameerthemc@gmail.com>
2023-10-04 14:46:07 +0530

    final code

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { copy, linkIcon, loader, tick } from '../assets'
 import { BiSubdirectoryRight } from "react-icons/bi"
 import { useLazyGetSummaryQuery } from '../services/article'
@@ -9,8 +9,18 @@ export default function Demo() {
         summary: ''
     })
     console.log(article)
+
     const [allArticles, setAllArticles] = useState([])
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
+    const [copied, setCopied] = useState('')
+
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+        if(articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage)
+        }
+    }, [])
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -19,10 +29,20 @@ export default function Demo() {
         if(data?.summary) {
             const newArticle = { ...article, summary: data.summary}
             setArticle(newArticle)
+
             const updatedAllArticles = [newArticle, ...allArticles]
             setAllArticles(updatedAllArticles)
+
+            localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
         }
     }
+
+    function handleCopy(copyUrl) {
+        setCopied(copyUrl)
+        navigator.clipboard.writeText(copyUrl)
+        // setTimeout(() => setCopied(false, 5000))
+    }
+
     return (
         <section className='mt-16 w-full max-w-xl'>
             <div className="flex flex-col w-full gap-2">
@@ -47,8 +67,60 @@ export default function Demo() {
                     </button>
                 </form>
                 {/* browser url history */}
+                <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+                    {
+                        allArticles.map((item, index) => {
+                            return (
+                                <div 
+                                    key={`link-${index}`} 
+                                    onClick={() => setArticle(item)}
+                                    className='link_card'
+                                >
+                                    <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                                        <img 
+                                            src={copied === item.url ? tick : copy} 
+                                            alt="copy_icon" 
+                                            className='w-[40%] h-[40%]' 
+                                        />
+                                    </div>
+                                    <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'> {item.url} </p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
             {/* display results */}
+            <div className="my-10 max-w-full flex justify-center items-center">
+                {
+                    isFetching ? (
+                        <img src={loader} alt='loader' className="w-20 h-20 object-contain" />
+                    ) : error ? (
+                        <p className='font-inter font-bold text-black text-center'> 
+                            there is an error  
+                            <br />
+                            <span>
+                                {error?.data?.error}
+                            </span>
+                        </p>
+                        
+                    ) : (
+                        article.summary && (
+                            <div className='flex flex-col gap-3'>
+                                <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                                    Article 
+                                    <span className='blue_gradient'>
+                                        Summary 
+                                    </span>
+                                </h2>
+                                <div className="summary_box">
+                                    <p className='font-inter font-medium text-sm text-gray-700'> {article.summary} </p>
+                                </div>
+                            </div>
+                        )
+                    )
+                }
+            </div>
         </section> 
     )
 }
